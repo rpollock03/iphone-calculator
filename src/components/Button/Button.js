307a80bb0c8bd05a63fb0757833a0fe5979a8ef5
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-07-15 08:23:14 +0100

    basic styling

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./Button.css"
+
+function Button(props) {
+
+    return (<div className="button">AC</div>)
+}
+
+
+export default Button
---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Button from "../Button/Button";
+import "./App.css"
+
+function App() {
+
+    return (
+        <div className="App">
+            <div className="top">8:12</div>
+            <div className="display">0</div>
+            <div className="buttons">AC</div>
+            <div className="bottom">-</div>
+
+        </div>)
+}
+
+export default App
